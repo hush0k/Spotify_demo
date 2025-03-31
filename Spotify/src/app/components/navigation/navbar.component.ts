bbb8c0bbe3669561053acd038b090c6c56dec3da
hush0k <127661635+hush0k@users.noreply.github.com>
2025-03-31 23:20:20 +0500

    Adding fonts and creating navigation bar

---
+++ Spotify/src/app/components/navigation/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
