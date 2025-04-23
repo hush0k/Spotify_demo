bed44c8c65704aa2882339702b3371c706eb8c5d
Doctyq <bissembayevd@list.ru>
2025-04-24 04:39:16 +0500

    s

---
+++ Spotify/src/app/components/header-premium/header-premium.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-premium',
+  standalone: true,
+  templateUrl: './header-premium.component.html',
+  styleUrl: './header-premium.component.scss'
+})
+export class HeaderPremiumComponent {
+
+}
