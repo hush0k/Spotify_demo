349235e9ac1b56d7dab63b89091f0d3b3842a459
Doctyq <bissembayevd@list.ru>
2025-04-24 05:58:27 +0500

    s

---
+++ Spotify/src/app/components/after-header-download/after-header-download.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-after-header-download',
+  imports: [],
+  templateUrl: './after-header-download.component.html',
+  styleUrl: './after-header-download.component.scss'
+})
+export class AfterHeaderDownloadComponent {
+
+}
